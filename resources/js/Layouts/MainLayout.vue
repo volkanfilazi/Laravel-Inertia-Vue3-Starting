0bb5198ce49ed9fdeb5589b0f31e5f7f1447e409
volkan filazi <volkanfilazi@hotmail.com>
2023-07-08 23:00:58 +0200

    added all staring files

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="root">
+    <Link href="/">Main Page</Link>
+    <Link href="/hello">Show Page</Link>
+    <Link href="/listing">Listing</Link>
+
+    <div>The page with time {{ timer }}</div>
+  </div>
+  <slot></slot>
+
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3'
+import {ref} from 'vue'
+
+const timer = ref(0)
+
+setInterval(() => timer.value++,1000)
+
+</script>
+
+<style>
+.root {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0px;
+}
+</style>
