3eaad0dfee9100f70a7c436f5454f04803df6eed
volkan filazi <volkanfilazi@hotmail.com>
2023-07-09 01:40:59 +0200

    now all elements are editable and database will accept all changes

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <ListingAddress :listing="listing"></ListingAddress>
+      <ListingAddress :listing="listing"></ListingAddress>
   </div>
 </template>
 
 <script setup>
 import ListingAddress from '../../Components/ListingAddress.vue'
+
 defineProps({
   listing: Object,
 })
