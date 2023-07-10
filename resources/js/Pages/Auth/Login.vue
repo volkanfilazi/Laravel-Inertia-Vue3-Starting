5b47020cfa6ea6f7b5b9b906a931b9c038ce96ad
volkan filazi <volkanfilazi@hotmail.com>
2023-07-10 23:49:20 +0200

    added sign-in logics and protected root functions

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -2,14 +2,14 @@
   <form @submit.prevent="login">
     <div class="w-full md:w-2/4 lg:w-1/4 space-y-5 mx-auto">
       <div>
-        <label for="email">E-mail (username)</label>
+        <label for="email">E-mail</label>
         <input type="text" name="" id="email" class="input" v-model="form.email">
-        <div class="input-error">Potential errors</div>
+        <div v-if="form.errors.email" class="input-error">{{ form.errors.email }}</div>
       </div>
       <div>
         <label for="password">Password</label>
         <input type="password" name="" id="password" class="input" v-model="form.password">
-        <div class="input-error">Potential errors</div>
+        <div v-if="form.errors.password" class="input-error">{{ form.errors.password }}</div>
       </div>
       <div>
         <button class="button-primary w-full" type="submit">Login</button>
